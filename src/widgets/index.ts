f95414a5a7b31a71f0d2cb577870bde97b7a44dd
Andrey Valiuk <andrey.valyuk.04@mail.ru>
2025-09-10 22:51:30 +0300

    Merge branch 'main' of https://github.com/Kamatochy04/books-swapper-client-

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export { default as Aside } from "./aside/Aside";
 export { default as GenderTabs } from "./genderTabs/GenderTabs";
 export { default as LessonCard } from "./lessonCard/LessonCard";
@@ -6,7 +5,3 @@ export { default as WeeklyProgress } from "./weeklyProgress/WeeklyProgress";
 export { default as DictionaryItem } from "./dictionaryItem/DictionaryItem";
 export { default as PartOfSpeechTabs } from "./partOfSpeechTabs/PartOfSpeechTabs";
 export { default as ModalFooter } from "./modalFooter/ModalFooter";
-=======
-export { default as Header } from "./header/Header";
-export { default as TaskCard } from "./taskCard/TaskCard";
->>>>>>> e75aba1f74a52c66e97ce7baae36eec88a4a4b1d
